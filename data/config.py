ef1ac6ce7a4c32359b69c5232564de91d1ba4a80
alcyonebots <evedhatachi@gmail.com>
2025-08-18 05:29:38 +0530

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,8 +6,8 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = env.str("8318054332:AAFWGD7VB197TS2ccqg8gvPlWFPkMwTB2Pg")  # Bot toekn
+ADMINS = env.list("6663845789")  # adminlar ro'yxati
+IP = env.str("178.62.254.82")  # Xosting ip manzili
 
 
